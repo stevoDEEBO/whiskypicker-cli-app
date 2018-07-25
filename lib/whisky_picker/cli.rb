3b4d44e1db50ba906d8083dbd212367daa6bfcc7
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-07-25 16:50:47 +0000

    added methods of countries and drill downs for scotch

--- lib/whisky_picker/cli.rb
+++ lib/whisky_picker/cli.rb
@@ -31,29 +31,115 @@ class WhiskyPicker::CLI
   def country_menu
     input = nil
     while input != "exit"
-      puts "Please enter the number of the country to explore or type exit to leave:"
+      puts "Please enter the number of the country to explore, list or type exit to leave:"
       input = gets.strip.downcase
       case input
       when "1"
-        puts "Let's explore Scotland"
+        scotch
       when "2"
-        puts "Let's explore Ireland"
+        irish
       when "3"
-        puts "Let's explore the United States of America"
+        american
       when "4"
-        puts "Let's explore Japan"
+        japanese
       when "5"
-        puts "Let's explore Canada"
+        canadian
       when "6"
-        puts "Let's explore other countries"
-#      when "exit"
-#        laters
-#      else
-#        puts "Didn't quite catch that..."
-#        countries
+        other
+      when "list"
+        countries
+      else
+        puts "Didn't quite catch that. Type countries or exit"
       end
     end
   end
+
+  def scotch
+    puts "Which type of Scotch would you like to explore?"
+    puts <<-DOC
+    1. Single Malt
+    2. Blended Malt
+    3. Blended
+    4. Grain
+    5. Organic
+    DOC
+    scotch_menu
+  end
+
+  def scotch_menu
+    input = nil
+    while input != "exit"
+      puts "Please enter number of selection or type list or exit."
+      input = gets.strip.downcase
+      case input
+      when "1"
+        scotch_single_malt
+      when "2"
+        scotch_blended_malt
+      when "3"
+        scotch_blended
+      when "4"
+        scotch_grain
+      when "5"
+        scotch_organic
+      when "list"
+        scotch
+      else
+        puts "Didn't quite catch that. Type list or exit"
+      end
+    end
+  end
+
+  def scotch_single_malt
+    puts "Let's explore single malt Scotch whiskies"
+    #scrape and list all single malt scotches
+  end
+
+  def scotch_blended_malt
+    puts "Let's explore blended malt Scotch whiskies"
+    #scrape and list all blended malt scotches
+  end
+
+  def scotch_blended
+    puts "Let's explore blended Scotch whiskies"
+    #scrape and list all blended scotches
+  end
+
+  def scotch_grain
+    puts "Let's explore grain Scotch whiskies"
+    #scrape and list all grain scotches
+  end
+
+  def scotch_organic
+    puts "Let's explore organic Scotch whiskies"
+    #scrape and list all organic scotches
+  end
+
+  def irish
+    puts "Let's explore the whiskies of Ireland"
+    #scrape and list all Irish whiskies
+  end
+
+  def american
+    puts "Let's explore the whiskies of the United States of America"
+    #scrape and list all American whiskies
+  end
+
+  def japanese
+    puts "Let's explore the whiskies of Japan"
+    #scrape and list all Japanese whiskies
+  end
+
+  def canadian
+    puts "Let's explore the whiskies of Canada"
+    #scrape and list all Canadian whiskies
+  end
+
+  def other
+    puts "Let's explore the whiskies of other countries"
+    #scrape and list all other whiskies
+  end
+
 
   def laters
     puts "Laters! Thanks for stopping by."
