1d1e83f9e9b95496c6b403191d1a52d1bfefd65b
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-07-25 15:45:02 +0000

    added search option listing methods countries & country_menu

--- lib/whisky_picker/cli.rb
+++ lib/whisky_picker/cli.rb
@@ -3,13 +3,18 @@ class WhiskyPicker::CLI
 
 #welcome user to gem and call list search options
   def call
-    puts "Welcome to Whisky Picker...ready to pick yer whisky?"
     pick_whiskies
   end
 
-#list search options by country of origin
+#list search options - leave open for now to expand by diff options- country, style, brand, etc
   def pick_whiskies
-    puts "Which country would you like to explore?"
+    puts "Welcome to Whisky Picker...ready to pick yer whisky?"
+    countries
+  end
+
+#list search options by country of origin
+  def countries
+    puts "Which country would you like to explore? Please enter number of selection."
     puts <<-DOC
     1. Scotch
     2. Irish
@@ -18,5 +23,10 @@ class WhiskyPicker::CLI
     5. Canadian
     6. Other
     DOC
+    country_menu
+  end
+
+  def country_menu
+    
   end
 end
