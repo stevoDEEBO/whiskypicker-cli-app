8e18c6125782e9beaa5cf1aa486e0c307fbe9aeb
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-07-27 07:58:23 +0000

    updated profile scraper

--- lib/whisky_picker/whisky_scraper.rb
+++ lib/whisky_picker/whisky_scraper.rb
@@ -25,11 +25,11 @@ class WhiskyPicker::Whisky_scraper
     whisky = {}
 
     whisky[:name] = profile.css("name-container h1").text.strip
-    #whisky[:region] = profile.css("").text.strip
-    whisky[:type] = profile.css(".name-container.properties li")[0].text.strip
+    whisky[:region_type] = profile.css(".name-container.properties li")[0].text.strip
     whisky[:proof] = profile.css(".strength").text.split(" / ")[1].strip
     whisky[:rating] = profile.css(".rating-stars-link").text.split("-").last.strip
     whisky[:description] = profile.css(".details-content-item").text.strip
+
 
     whisky
   end
